f7a28a7dc04e72619720ecffd9808589686234bb
Andre-404 <andreamaric.rv@gmail.com>
2026-01-23 20:06:15 +0100

    playwright fixes

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -48,6 +48,8 @@ test('admin dish creation of already existing dish', async ({ page, isMobile }) 
   await page.getByRole('textbox', { name: 'Naziv jela' }).fill('Testno jelo');
   await page.getByRole('button', { name: 'Kreiraj jelo' }).click();
   await expect(page.getByText('A dish with this name already')).toBeVisible();
+  await page.getByRole('button', { name: 'Open user menu' }).click();
+  await page.getByRole('menuitem', { name: 'Sign out' }).click();
 });
 
 
@@ -59,6 +61,8 @@ test('wrong login for worker', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Lozinka' }).fill('test');
   await page.getByRole('button', { name: 'Prijavi se' }).click();
   await expect(page.getByText('Neispravno korisničko ime ili')).toBeVisible();
+  await page.getByRole('button', { name: 'Open user menu' }).click();
+  await page.getByRole('menuitem', { name: 'Sign out' }).click();
 }); 
 
 test('manual redirect test', async ({ page }) => {
@@ -69,4 +73,6 @@ test('manual redirect test', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Lozinka' }).fill('worker');
   await page.getByRole('button', { name: 'Prijavi se' }).click();
   await page.goto('http://employee.localhost:3000/admin');
+  await page.getByRole('button', { name: 'Open user menu' }).click();
+  await page.getByRole('menuitem', { name: 'Sign out' }).click();
 });
